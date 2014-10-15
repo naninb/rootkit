cd9e27d90e39a8e792ac656b944df47ea8b80b2a
Nani <Nani@Naniteki-MacBook-Air.local>
2014-10-15 10:19:03 +0200

    Initial Commit

---
+++ rootkit/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  rootkit
+//
+//  Created by Nani on 14/10/15.
+//  Copyright (c) 2014年 Nani. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
